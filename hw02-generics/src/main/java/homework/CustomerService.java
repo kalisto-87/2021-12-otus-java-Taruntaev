59c4807850a341d00dfef24cce16f70721cd4e6b
kalisto <kalisto-87@mail.ru>
2022-04-14 00:15:46 +0300

    The name of a method has been changed

--- hw02-generics/src/main/java/homework/CustomerService.java
+++ hw02-generics/src/main/java/homework/CustomerService.java
@@ -14,19 +14,19 @@ public class CustomerService {
     public Map.Entry<Customer, String> getSmallest() {
         //Возможно, чтобы реализовать этот метод, потребуется посмотреть как Map.Entry сделан в jdk
         var mapEntry = map.firstEntry();
-        return newCustomer(mapEntry);
+        return CopyEntry(mapEntry);
     }
 
     public Map.Entry<Customer, String> getNext(Customer customer) {
         var mapEntry = map.higherEntry(customer);
-        return newCustomer(mapEntry);
+        return CopyEntry(mapEntry);
     }
 
     public void add(Customer customer, String data) {
         var mapEntry = map.put(customer, data);
     }
 
-    private Map.Entry<Customer, String> newCustomer(Map.Entry<Customer, String> entry) {
+    private Map.Entry<Customer, String> CopyEntry(Map.Entry<Customer, String> entry) {
         if (entry == null) return null;
         return Map.entry(
                 new Customer(entry.getKey().getId(), entry.getKey().getName(), entry.getKey().getScores()),
